aa00b57870b0ceff5f62773387a1a8bde484463e
Dmitriy Grinevich <Dmitriy.Grinevich@quorumsoftware.com>
2023-07-12 14:38:24 +0300

    Finish with bdd

--- EABddTests/Startup.cs
+++ EABddTests/Startup.cs
@@ -1,18 +1,33 @@
 ﻿using EABddTests.Pages;
 using Microsoft.Extensions.DependencyInjection;
 using EATestBase.Driver;
+using SolidToken.SpecFlow.DependencyInjection;
 
 namespace EABddTests;
 
-public class Startup
+public static class Startup
 {
-    public void ConfigureServices(IServiceCollection services)
+    // public void ConfigureServices(IServiceCollection services)
+    // {
+    //     services.AddScoped<IDriverFixture, DriverFixture>();
+    //     services.AddScoped<IBrowserDriver, BrowserDriver>();
+    //     services.AddScoped<IHomePage, HomePage>();
+    //     services.AddScoped<IProductPage, ProductPage>();
+    //
+    //     services.UseWebDriverInitializer();
+    // }
+
+    [ScenarioDependencies]
+    public static IServiceCollection CreateServices()
     {
+        var services = new ServiceCollection();
+
         services.AddScoped<IDriverFixture, DriverFixture>();
         services.AddScoped<IBrowserDriver, BrowserDriver>();
         services.AddScoped<IHomePage, HomePage>();
         services.AddScoped<IProductPage, ProductPage>();
 
         services.UseWebDriverInitializer();
+        return services;
     }
 }
